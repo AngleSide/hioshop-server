5445d25cf0eb11bbd20d60e16376d8dc7296921b
林智清 <3370942916@qq.com>
2025-10-02 21:35:31 +0800

    fix: 更新数据库配置

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
     database: 'hiolabsDB',
     prefix: 'hiolabs_',
     encoding: 'utf8mb4',
-    host: '127.0.0.1',
+    host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '123123123',
+    password: 'F29A99E8-4E64-4728-813A-3725EDBAF376',
     dateStrings: true
 };
